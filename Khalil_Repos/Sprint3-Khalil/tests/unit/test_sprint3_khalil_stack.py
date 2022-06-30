c14e7393f02c4233907cc37d84efa0ddbe7bebc3
Ubuntu <ubuntu@ip-172-31-36-16.us-east-2.compute.internal>
2022-06-30 04:18:06 +0000

    Pipeline v2

--- Khalil_Repos/Sprint3-Khalil/tests/unit/test_sprint3_khalil_stack.py
+++ Khalil_Repos/Sprint3-Khalil/tests/unit/test_sprint3_khalil_stack.py
@@ -1,6 +1,6 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-
+import pytest
 from sprint3_khalil.sprint3_khalil_stack import Sprint3KhalilStack
 
 # example tests. To run these tests, uncomment this file along with the example
@@ -10,12 +10,21 @@ from sprint3_khalil.sprint3_khalil_stack import Sprint3KhalilStack
 #     stack = Sprint3KhalilStack(app, "sprint3-khalil")
 #     template = assertions.Template.from_stack(stack)
 
-# To ignore warnings:  https://stackoverflow.com/questions/40710094/how-to-suppress-py-test-internal-deprecation-warnings
-# pytest path-to-test-folder -W ignore::DeprecationWarning  OR pytest -W ignore::DeprecationWarning
-def test_create_lambda():
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+
+@pytest.fixture
+# https://docs.pytest.org/en/6.2.x/fixture.html
+def template():
     app = core.App()
     stack = Sprint3KhalilStack(app, "sprint3-khalil")
     template = assertions.Template.from_stack(stack)
+    return template
+
+# To ignore warnings:  https://stackoverflow.com/questions/40710094/how-to-suppress-py-test-internal-deprecation-warnings
+# pytest path-to-test-folder -W ignore::DeprecationWarning  OR pytest -W ignore::DeprecationWarning
+def test_create_lambda(template):
     template.resource_count_is("AWS::Lambda::Function",2)   #2 or 4
 
 # def test_create_DYnamo():
@@ -34,11 +43,3 @@ def test_create_lambda():
     # template.resource_count_is("AWS::IAM::Role",1)
 
 
-# def test_sqs_queue_created():
-#     app = core.App()
-#     stack = Sprint3KhalilStack(app, "sprint3-khalil")
-#     template = assertions.Template.from_stack(stack)
-
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
