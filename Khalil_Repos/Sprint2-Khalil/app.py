59ee54ba72d18ddf9fdd35392d66179adedb0a88
EC2 Default User <ec2-user@ip-172-31-35-194.us-east-2.compute.internal>
2022-08-13 11:30:44 +0000

    Sprint2 Final

--- Khalil_Repos/Sprint2-Khalil/app.py
+++ Khalil_Repos/Sprint2-Khalil/app.py
@@ -7,8 +7,12 @@ from sprint2_khalil.sprint2_khalil_stack import Sprint2KhalilStack
 
 
 app = cdk.App()
+
+# https://docs.aws.amazon.com/cdk/v2/guide/tagging.html
+# Tags are informational key-value elements that you can add to constructs in your AWS CDK app. A tag applied to a given construct also applies to all of its taggable children
 cdk.Tags.of(app).add("cohort" , "Pegasus")
 cdk.Tags.of(app).add("name" , "Khalilullah")
+
 Sprint2KhalilStack(app, "Sprint2KhalilStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
