24a803aa3c77eec4f97a955329ed310fff793b9d
EC2 Default User <ec2-user@ip-172-31-35-194.us-east-2.compute.internal>
2022-08-17 13:33:14 +0000

    Sprint6 Day7

--- Khalil_Repos/Sprint6-Khalil/Day-7/resource/s3Files.py
+++ Khalil_Repos/Sprint6-Khalil/Day-7/resource/s3Files.py
@@ -5,8 +5,6 @@ import sys
 
 s3 = boto3.client('s3')         # Set boto3 client
 
-
-
 # s3.download_file('sprintonekhalil', 'file.py', '/tmp/websites.py')       #must be 'tmp' beacuse it is a root folder
 # sys.path.insert(1, '/tmp')                  # to enter in path 'tmp'
 
